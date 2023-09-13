c097ea9add16aef02557a33b694ac141b32f373f
rkundan <rahulkundan60@gmail.com>
2023-09-14 01:55:30 +0530

    initial commit

---
+++ README.md
@@ -0,0 +1,22 @@
+This is a simple calculator app created with [Next.js](https://nextjs.org/).
+It supports parenthesized expressions as well as the following numerical operators
+* addition
+* subtraction
+* multiplication
+* divison
+
+A modified version of the [Shunting yard algorithm](https://en.wikipedia.org/wiki/Shunting_yard_algorithm) is used for interpreting arithmetic expressions
+## Getting Started
+### Run the development server:
+
+```bash
+yarn dev
+```
+
+Open [http://localhost:3000](http://localhost:3000) with your browser to see the result.
+
+### Run tests
+```bash
+yarn build
+yarn test
+```
---
+++ pages/index.tsx
@@ -0,0 +1,57 @@
+import type { NextPage } from 'next';
+import { useState } from 'react';
+import { operators, parentheses, evaluateInput } from '../lib/arithmetic';
+
+
+const Home: NextPage = () => {
+  const [input, setInput] = useState('');
+
+  return (
+    <>
+      <div className='calculator'>
+        <div className='screen'>{input}</div>
+        <div className='keyboard'>
+          {[1,2,3,4,5,6,7,8,9,0].map(number => (
+            <button
+              value={number}
+              onClick={() => setInput(`${input}${number}`)}
+              key={number}
+            >
+              {number}
+            </button>
+          ))}
+          {operators.map((symbol, index) => (
+            <button
+              value={symbol}
+              onClick={() => setInput(`${input} ${symbol} `)}
+              key={index}
+            >
+              {symbol}
+            </button>
+          ))}
+          <button
+            value={'='}
+            onClick={() => setInput(evaluateInput(input.split(' ')))}
+          >=</button>
+          {parentheses.map((paren, index) => (
+            <button
+              value={paren}
+              onClick={() =>
+                setInput(
+                  paren === '(' ? `${input}${paren} ` : `${input} ${paren}`
+                )
+              }
+              key={index}
+            >
+              {paren}
+            </button>
+          ))}
+          <button value={'C'} onClick={() => setInput('')}>
+            AC
+          </button>
+        </div>
+      </div>
+    </>
+  );
+};
+export default Home;
